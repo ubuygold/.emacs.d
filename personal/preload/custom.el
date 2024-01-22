c7d7a2e367ddbdda87fc215769b11f2cc30d0c71
ubuygold <ubuygold@live.com>
2024-01-22 16:30:56 +0800

    Initail commit

---
+++ personal/preload/custom.el
@@ -0,0 +1,8 @@
+(eval-after-load 'company
+  '(progn
+     (define-key company-mode-map (kbd "C-:") 'helm-company)
+     (define-key company-active-map (kbd "C-:") 'helm-company)))
+
+(use-package lsp-tailwindcss
+  :init
+  (setq lsp-tailwindcss-add-on-mode t))
